35070b8874a349e47365fcb98a2e63223d57ff65
David <34396308+David548219@users.noreply.github.com>
2021-12-16 15:40:04 +0300

    Added bad split protection
    Changed input pin and added pull up

--- asvt_course_money/update_loop.py
+++ asvt_course_money/update_loop.py
@@ -5,6 +5,7 @@ import RPi.GPIO as gpio
 
 async def update_loop(callback, *, verbose=False):
     # Initialising counter
+    last_pulse_count = 0
     pulse_count = 0
 
     def pulse(channel):
@@ -13,15 +14,17 @@ async def update_loop(callback, *, verbose=False):
 
     # GPIO setup
     gpio.setmode(gpio.BCM)
-    gpio.setup(2, gpio.IN)
-    gpio.add_event_detect(2, gpio.RISING, callback=pulse)
+    gpio.setup(4, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.add_event_detect(4, gpio.RISING, callback=pulse)
 
     # Update loop
     while True:
-        await sleep(0.06)
-        if pulse_count == 0:
+        await sleep(0.4)
+        if pulse_count == 0 or last_pulse_count != pulse_count:
+            last_pulse_count = pulse_count
             continue
         callback(pulse_count)
         if verbose:
             print(f"PULSE {pulse_count}")
+        last_pulse_count = 0
         pulse_count = 0
