c56bef3f713cddff6b468b7c1288b4b703a9a9c4
David <34396308+David548219@users.noreply.github.com>
2021-12-17 20:13:38 +0300

    Added init to sample startup

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from asyncio import new_event_loop
 
-from asvt_course_money.update_loop import update_loop
+from asvt_course_money.update_loop import update_loop, initialise_pins
 
 
 def example_callback(pulse_value):
@@ -8,6 +8,7 @@ def example_callback(pulse_value):
 
 
 if __name__ == "__main__":
+    initialise_pins()
     loop = new_event_loop()
     loop.create_task(update_loop(example_callback, verbose=True))
     loop.run_forever()
